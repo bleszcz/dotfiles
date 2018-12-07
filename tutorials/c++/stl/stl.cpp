dfc61ba8012c18a5186f073dfeb3f5a1eacde342
boczulak <marcin.boczulak@nokia.com>
2018-12-07 13:23:10 +0100

    added queries

--- tutorials/c++/stl/stl.cpp
+++ tutorials/c++/stl/stl.cpp
@@ -4,6 +4,7 @@
 #include <iterator>
 #include <sstream>
 #include <array>
+#include <random>
 
 using namespace std;
 
@@ -304,22 +305,395 @@ void partitions()
 
 void parmutations()
 {
-
-    std::vector<int> v{3, 5, 2, 1, 4, 3, 3, 10, 5, 8, 9, 1, 2, 3};
+    std::vector<int> v{3, 5, 2,  4, 7, 10, 5, 8, 9, 1, 7};
     std::vector<dupa> dv{{1, 2}, {3, 5}, {4, 6}, {1, 2}};
     std::cout << "initially v: " << std::endl;
     std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ", " ));
     std::cout << "\n";
 
+    // insertion sort
+    for (auto i = v.begin(); i != v.end(); ++i) {
+        std::rotate(std::upper_bound(v.begin(), i, *i), i, i+1);
+    }
 
+    std::cout << "after sort:\n";
+    for (int n: v)  std::cout << n << ' ';
+    std::cout << '\n';
+
+    /* rotate
+     *
+     * Complexity
+     *
+     *  Linear in the distance between first and last
+     *
+     * Possible implementation:
+     *
+        template <class ForwardIt>
+        ForwardIt rotate(ForwardIt first, ForwardIt n_first, ForwardIt last)
+        {
+            if(first == n_first) return last;
+            if(n_first == last) return first;
+
+            ForwardIt next = n_first;
+
+            do {
+                std::iter_swap(first++, next++);
+                if (first == n_first) n_first = next;
+            }
+            while (next != last);
+
+            ForwardIt ret = first;
+
+            for(next = n_first; next != last; ) {
+                std::iter_swap(first++, next++);
+                if(first == n_first) n_first = next;
+                else if(next == last) next = n_first;
+            }
+
+            return ret;
+        }
+     *
+     */
+
+    std::rotate(v.begin(), v.begin()+2, v.end());
+    std::cout << "simple rotate left by two elements: " << "\n";
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << std::endl;
+
+    std::rotate(v.begin(), v.begin()+4, v.end());
+    std::cout << "simple next rotate left by four elements: " << "\n";
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << std::endl;
+
+    std::cout << "simple rotate right by one element: " << "\n";
+    std::rotate(v.rbegin(), v.rbegin()+1, v.rend());
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << std::endl;
+
+    /* shuffle
+     *
+    * random_shuffle - deprecated
+     *
+     * Complexity
+     *
+     *  Linear in the distance between first and last
+     *
+     * Possible implementation:
+     *
+        template<class RandomIt, class URBG>
+        void shuffle(RandomIt first, RandomIt last, URBG&& g)
+        {
+            typedef typename std::iterator_traits<RandomIt>::difference_type diff_t;
+            typedef std::uniform_int_distribution<diff_t> distr_t;
+            typedef typename distr_t::param_type param_t;
+
+            distr_t D;
+            diff_t n = last - first;
+            for (diff_t i = n-1; i > 0; --i) {
+                using std::swap;
+                swap(first[i], first[D(g, param_t(0, i))]);
+            }
+        }
+    */
+
+    v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    std::cout << "initially v: " << std::endl;
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ", " ));
+    std::cout << "\n";
+
+    std::random_device rd;
+    std::mt19937 g(rd());
+
+    std::shuffle(v.begin(), v.end(), g);
+    std::cout << "after shuffle:\n";
+    std::copy(v.begin(), v.end(), ostream_iterator<int>(std::cout, ", "));
+    std::cout << std::endl;
+
+    /* reverse
+     *
+     * 1) Reverses the order of the elements in the range [first, last)
+     *
+     * Possible implementation:
+     *
+        template<class BidirIt>
+        void reverse(BidirIt first, BidirIt last)
+        {
+            while ((first != last) && (first != --last)) {
+                std::iter_swap(first++, last);
+            }
+        }
+    */
+
+    v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::cout << "initially v: " << std::endl;
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ", " ));
+    std::cout << "\n";
+
+    std::reverse(v.begin(), v.end());
+
+    std::cout << "after reverse\n";
+    std::copy(v.begin(), v.end(), ostream_iterator<int>(std::cout, ", "));
+    std::cout << std::endl;
+
+
+    //v = {3, 5, 2,  4, 7, 10, 5, 8, 9, 1, 7};
+    v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    std::cout << "initialy v: " << std::endl;
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ", " ));
+    std::cout << "\n";
+
+
+
+    /* next_permutation
+     *
+     * Complexity
+     *
+     *  At most N/2 swaps, where N = std::distance(first, last).
+     *  Averaged over the entire sequence of permutations, typical implementations
+     *  use about 3 comparisons and 1.5 swaps per call.
+     *
+     * Possible implementation:
+
+        template<class BidirIt>
+        bool next_permutation(BidirIt first, BidirIt last)
+        {
+            if (first == last) return false;
+            BidirIt i = last;
+            if (first == --i) return false;
+
+            while (true) {
+                BidirIt i1, i2;
+
+                i1 = i;
+                if (*--i < *i1) {
+                    i2 = last;
+                    while (!(*i < *--i2))
+                        ;
+                    std::iter_swap(i, i2);
+                    std::reverse(i1, last);
+                    return true;
+                }
+                if (i == first) {
+                    std::reverse(first, last);
+                    return false;
+                }
+            }
+        }
+
+     *
+     *
+     *
+    */
+
+    std::next_permutation(v.begin(), v.end());
+
+    std::cout << "after next_permutation:\n";
+    std::copy(v.begin(), v.end(), ostream_iterator<int>(std::cout, ", "));
+    std::cout << std::endl;
+
+    std::next_permutation(v.begin(), v.end());
+
+    std::cout << "after next_permutation:\n";
+    std::copy(v.begin(), v.end(), ostream_iterator<int>(std::cout, ", "));
+    std::cout << std::endl;
+
+    std::string s = "aba";
+    std::sort(s.begin(), s.end());
+    do {
+        std::cout << s << '\n';
+    } while(std::next_permutation(s.begin(), s.end()));
+
+    /* prev_permutation
+     *
+     * Complexity
+     *
+     * At most (last-first)/2 swaps.
+     * Averaged over the entire sequence of permutations,
+     * typical implementations use about 3 comparisons and 1.5 swaps per call.
+     *
+     * Possible implementation:
+
+
+        template<class BidirIt>
+        bool prev_permutation(BidirIt first, BidirIt last)
+        {
+            if (first == last) return false;
+            BidirIt i = last;
+            if (first == --i) return false;
+
+            while (1) {
+                BidirIt i1, i2;
+
+                i1 = i;
+                if (*i1 < *--i) {
+                    i2 = last;
+                    while (!(*--i2 < *i))
+                        ;
+                    std::iter_swap(i, i2);
+                    std::reverse(i1, last);
+                    return true;
+                }
+                if (i == first) {
+                    std::reverse(first, last);
+                    return false;
+                }
+            }
+        }
+
+     *
+     *
+     *
+    */
+
+    std::prev_permutation(v.begin(), v.end());
+
+    std::cout << "after prev_permutation:\n";
+    std::copy(v.begin(), v.end(), ostream_iterator<int>(std::cout, ", "));
+    std::cout << std::endl;
+
+    s="abc";
+    std::sort(s.begin(), s.end(), std::greater<char>());
+    do {
+        std::cout << s << ' ';
+    } while(std::prev_permutation(s.begin(), s.end()));
+    std::cout << '\n';
 }
+
+void queries()
+{
+    std::vector<int> v{3, 5, 2,  4, 7, 10, 5, 8, 9, 1, 7};
+    std::vector<dupa> dv{{1, 2}, {3, 5}, {4, 6}, {1, 2}};
+    std::cout << "initially v: " << std::endl;
+    std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ", " ));
+    std::cout << "\n";
+
+    /* count
+     *
+     * Complexity
+     *
+     *
+     *
+     * Possible implementation:
+     *
+     */
+
+    std::count(v.begin(), v.end(), 3);
+
+    /* accumulate
+     *
+     * Complexity
+     *
+     *
+     *
+     * Possible implementation:
+     *
+     */
+
+    /*auto result = */std::accumulate(v.begin(), v.end(), 1 , std::multiplies<int>());
+
+    /* reduce    since c++17
+     *
+     * Complexity
+     *
+     *
+     *
+     * Possible implementation:
+     *
+     */
+
+ //   auto sum = std::reduce(v.begin(), v.end());
+
+    /* partial_sum
+     *
+     * Complexity
+     *
+     *
+     *
+     * Possible implementation:
+     *
+     */
+
+    /*auto result = */std::partial_sum(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
+
+    /* inclusive_scan   since c++17
+     *
+     * Complexity
+     *
+     *
+     *
+     * Possible implementation:
+     *
+     */
+
+    /* exclusive_scan since c++17
+     *
+     * Complexity
+     *
+     *
+     *
+     * Possible implementation:
+     *
+     */
+
+    //std::exclusive_scan(v.begin(), v.end());
+
+    /* inner_product
+     *
+     * Complexity
+     *
+     *
+     *
+     * Possible implementation:
+     *
+     */
+
+    std::vector<int> a{0, 1, 2, 3, 4};
+    std::vector<int> b{5, 4, 2, 3, 1};
+
+    int r1 = std::inner_product(a.begin(), a.end(), b.begin(), 0);
+    std::cout << "Inner product of a and b: " << r1 << '\n';
+
+    int r2 = std::inner_product(a.begin(), a.end(), b.begin(), 0,
+                                std::plus<>(), std::equal_to<>());
+
+    std::cout << "Number of pairwise matches between a and b: " <<  r2 << '\n';
+
+    /* adjacent_difference()
+     *
+     * Complexity
+     *
+     *
+     *
+     * Possible implementation:
+     *
+     */
+
+    std::adjacent_difference(v.begin(), v.end(), v.begin());
+
+    /* sample since c++17
+     *
+     * Complexity
+     *
+     *
+     *
+     * Possible implementation:
+     *
+     */
+
+    //std::sample();
+}
+
+
 
 int main()
 {
 //    heaps();
 //    sorts();
 //    partitions();
-    parmutations();
+//    parmutations();
+    queries();
 
 
     return 0;
